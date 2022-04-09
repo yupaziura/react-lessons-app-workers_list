bab348aa4ec46f45eca0e30b1d8b3e6d4d67ba49
YuliiaPaziura <yuliiapaziura@gmail.com>
2022-04-09 16:32:44 +0300

    form create controlled elements

--- src/components/employees-form-add/employees-form-add.js
+++ src/components/employees-form-add/employees-form-add.js
@@ -1,24 +1,50 @@
+import {Component} from 'react';
 import './employees-form-add.css';
 
 
-const EmployeesFormAdd = () => {
-    return (
-        <div className="app-add-form">
-            <h3>Add employee</h3>
-            <form
-                className="add-form d-flex">
-                <input type="text"
-                    className="form-control new-post-label"
-                    placeholder="Name" />
-                <input type="number"
-                    className="form-control new-post-label"
-                    placeholder="Payrate" />
+class EmployeesFormAdd extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            name: '',
+            payrate: ''
+        }
+    }
 
-                <button type="submit"
-                        className="btn btn-outline-light">Add</button>
-            </form>
-        </div>
-    )
+    onValue = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+
+    render () {
+        return (
+            <div className="app-add-form">
+                <h3>Add employee</h3>
+                <form
+                    className="add-form d-flex">
+                    <input type="text"
+                        className="form-control new-post-label"
+                        placeholder="Name"
+                        name = 'name'
+                        onChange={this.onValue}
+                        valuse={this.state.name}
+                        />
+                    <input type="number"
+                        className="form-control new-post-label"
+                        placeholder="Payrate" 
+                        name = 'payrate'
+                        onChange={this.onValue}
+                        valuse={this.state.payrate}
+                        />
+    
+                    <button type="submit"
+                            className="btn btn-outline-light">Add</button>
+                </form>
+            </div>
+        )
+    
+    }
 }
 
 export default EmployeesFormAdd;
